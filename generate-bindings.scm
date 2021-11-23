ca67b5a43b2fefa5f986dad3fe682a02cdcbf2ec
Peter Elliott <pelliott@ualberta.ca>
2021-11-22 19:46:40 -0700

    more complicated type support for structs and pointers.
    I've also added a function blacklist for stuff that will be handwritten
    raylib-guile.c compiles now!

--- generate-bindings.scm
+++ generate-bindings.scm
@@ -30,16 +30,80 @@
                     (filter pair? (cddr enum)))))
        (filter pair? (cddr (assoc 'Enums raylibAPI)))))
 
+;; these functions don't appear in the generated bindings.
+;; some of them should be re-added when our generation gets smarter, and some will be hand written.
+(define fn-blacklist
+  '("GetWindowHandle"
+    "SetShaderValue"
+    "SetShaderValueV"
+    "TraceLog"
+    "MemAlloc"
+    "MemRealloc"
+    "MemFree"
+    "SetTraceLogCallback"
+    "SetLoadFileDataCallback"
+    "SetSaveFileDataCallback"
+    "SetLoadFileTextCallback"
+    "SetSaveFileTextCallback"
+    "LoadFileData"
+    "UnloadFileData"
+    "SaveFileData"
+    "LoadFileText"
+    "UnloadFileText"
+    "SaveFileText"
+    "GetDirectoryFiles"
+    "GetDroppedFiles"
+    "CompressData"
+    "DecompressData"
+    "EncodeDataBase64"
+    "DecodeDataBase64"
+    "UpdateCamera"
+    "DrawLineStrip"
+    "DrawTriangleFan"
+    "DrawTriangleStrip"
+    "CheckCollisionLines"
+    "LoadImageAnim"
+    "LoadImageColors"
+    "LoadImagePalette"
+    "UpdateTexture"
+    "UpdateTextureRec"
+    "GetPixelColor"
+    "SetPixelColor"
+    "LoadFontEx"
+    "LoadFontFromMemory"
+    "LoadFontData"
+    "GenImageFontAtlas"
+    "LoadCodepoints"
+    "UnloadCodepoints"
+    "GetCodepoint"
+    "CodepointToUTF8"
+    "TextCodepointsToUTF8"
+    "TextCopy"
+    "TextFormat"
+    "TextJoin"
+    "TextSplit"
+    "TextAppend"
+    "UpdateMeshBuffer"
+    "LoadMaterials"
+    "LoadModelAnimations"
+    "UnloadModelAnimations"
+    "UpdateSound"
+    "LoadWaveSamples"
+    "UnloadWaveSamples"
+    "UpdateAudioStream"))
+
+
 ;; functions is of the form ((name rettype (type arg) ...) ...)
 (define functions
-  (map (lambda (fn)
-         (cons (cadr (assoc 'name (cdadr fn)))
-               (cons (cadr (assoc 'retType (cdadr fn)))
-                     (map (lambda (arg)
-                            (cons (cadr (assoc 'name (cdadr arg)))
-                                  (cadr (assoc 'type (cdadr arg)))))
-                          (filter pair? (cddr fn))))))
-       (filter pair? (cddr (assoc 'Functions raylibAPI)))))
+  (filter (lambda (fn) (not (member (car fn) fn-blacklist)))
+          (map (lambda (fn)
+                 (cons (cadr (assoc 'name (cdadr fn)))
+                       (cons (cadr (assoc 'retType (cdadr fn)))
+                             (map (lambda (arg)
+                                    (cons (cadr (assoc 'name (cdadr arg)))
+                                          (cadr (assoc 'type (cdadr arg)))))
+                                  (filter pair? (cddr fn))))))
+               (filter pair? (cddr (assoc 'Functions raylibAPI))))))
 
 (define genlocal ((lambda ()
                     (define val 0)
@@ -47,27 +111,65 @@
                       (set! val (+ 1 val))
                       (format #f "v~a" val)))))
 
+;; TODO: add this to raylib's api parser upstream.
+(define (resolve-typedef type)
+  (define aliases
+    '(("Quaternion" . "Vector4")
+      ("Texture2D" . "Texture")
+      ("TextureCubemap" . "Texture")
+      ("RenderTexture2D" . "RenderTexture")
+      ("Camera" . "Camera3D")))
+  (define entry (assoc type aliases))
+  (if entry (cdr entry) type))
+
 (define (sanitize-type type)
-  (string-replace-substring type "unsigned " "u"))
+  (string-replace-substring
+   (string-replace-substring
+    (resolve-typedef type) "unsigned " "u")
+   "const " ""))
+
+(define (deptr-type type)
+  (if (and (>= (string-length type) 2)
+           (string= (substring type (- (string-length type) 2)) " *"))
+      (substring type 0 (- (string-length type) 2))
+      ""))
 
 (define (scm->c port type expr)
+  (define stype (sanitize-type type))
+  (define dtype (sanitize-type (deptr-type type)))
   (cond
-   ((string= type "const char *")
+   ((or (string= stype "char *") (string= stype "uchar *"))
     (let ((local (genlocal)))
       (format port "    char *~a = scm_to_utf8_stringn(~a, NULL);\n    scm_dynwind_free(~a);\n" local expr local)
       local))
-   (else (format #f "scm_to_~a(~a)" (sanitize-type type) expr))))
+   ((member stype struct-names)
+    (format port "    scm_assert_foreign_object_type(rgtype_~a, ~a);\n" stype expr)
+    (format #f "*(~a*)scm_foreign_object_ref(~a, 0)" stype expr))
+   ((member dtype struct-names)
+    (format port "    scm_assert_foreign_object_type(rgtype_~a, ~a);\n" dtype expr)
+    (format #f "scm_foreign_object_ref(~a, 0)" expr))
+   ((string= stype "float") (format #f "scm_to_double(~a)" expr))
+   (else (format #f "scm_to_~a(~a)" stype expr))))
 
 (define (c->scm port type expr)
+  (define stype (sanitize-type type))
+  (define dtype (sanitize-type (deptr-type type)))
   (cond
-   ((string= type "const char *")
+   ((or (string= stype "char *") (string= stype "uchar *"))
     (format #f "scm_from_utf8_string(~a)" expr))
    ((string= type "void")
     (format #f "(~a, SCM_UNSPECIFIED)" expr))
-   (else (format #f "scm_from_~a(~a)" (sanitize-type type) expr))))
+   ((member stype struct-names)
+    (let ((local (genlocal)))
+      (format port "    void *~a = scm_gc_malloc_pointerless(sizeof(~a), \"raylib-guile ptr\");\n" local stype)
+      (format port "    ~a ~a_data = ~a;\n    memcpy(~a, &~a_data, sizeof(~a));\n"
+              stype local expr local local stype)
+      (format #f "scm_make_foreign_object_1(rgtype_~a, ~a)" stype local)))
+   ((string= stype "float") (format #f "scm_from_double(~a)" expr))
+   (else (format #f "scm_from_~a(~a)" stype expr))))
 
 (define (generate-function f port)
-  (format port "SCM raylib_guile_~a(~{SCM ~a~^, ~}) {\n" (car f) (map car (cddr f)))
+  (format port "SCM rgfun_~a(~{SCM ~a~^, ~}) {\n" (car f) (map car (cddr f)))
   (format port "    scm_dynwind_begin(0);\n")
   (format port "    SCM result = ~a;\n"
           (c->scm port (cadr f)
@@ -79,12 +181,13 @@
   (format port "    return result;\n")
   (format port "}\n\n"))
 
-
 ;; generate c guile bindings
 (call-with-output-file coutput
   (lambda (port)
-    (format port "#include <raylib.h>\n#include <libguile.h>\n\n")
+    (format port "#include <raylib.h>\n#include <libguile.h>\n#include <string.h>\n")
 
-    ;; generate t
-    (for-each (lambda (f) (generate-function f port) functions)
-              functions)))
+    (format port "\n// struct slots\n")
+    (for-each (lambda (s) (format port "static SCM rgtype_~a;\n" s)) struct-names)
+
+    (format port "\n// function definitions\n")
+    (for-each (lambda (f) (generate-function f port)) functions)))
